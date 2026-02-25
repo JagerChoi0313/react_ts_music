7b677ccb88ef6dc77bf2f80be3046d2277b39a1d
JagerChoi <17322905680@qq.com>
2026-02-25 12:19:47 +0800

    发现音乐--顶部的导航菜单的实现过程

---
+++ src/assets/data/local-data.ts
@@ -0,0 +1,56 @@
+export const footLink = [
+    {
+        "title": "服务条款",
+        "link": "https://st.music.163.com/official-terms/service"
+    },
+    {
+        "title": "隐私政策",
+        "link": "https://st.music.163.com/official-terms/privacy"
+    },
+    {
+        "title": "儿童隐私政策",
+        "link": "https://st.music.163.com/official-terms/children"
+    },
+    {
+        "title": "版权投诉",
+        "link": "https://music.163.com/st/staticdeal/complaints.html"
+    },
+    {
+        "title": "投资者关系",
+        "link": "#"
+    },
+    {
+        "title": "广告合作",
+        "link": "#"
+    },
+    {
+        "title": "联系我们",
+        "link": "#"
+    }
+]
+export const discoverMenu = [
+    {
+        "title": "推荐",
+        "link": "/discover/recommend"
+    },
+    {
+        "title": "排行榜",
+        "link": "/discover/ranking"
+    },
+    {
+        "title": "歌单",
+        "link": "/discover/songs"
+    },
+    {
+        "title": "播客",
+        "link": "/discover/djradio"
+    },
+    {
+        "title": "歌手",
+        "link": "/discover/artist"
+    },
+    {
+        "title": "新碟上架",
+        "link": "/discover/album"
+    }
+]
---
+++ src/views/discover/c-cpns/nav-bar/index.tsx
@@ -0,0 +1,31 @@
+import React, { memo } from 'react'
+import type { FC, ReactNode } from 'react'
+import { NavWrapper } from './style'
+import {NavLink} from 'react-router-dom'
+import {discoverMenu} from '@/assets/data/local-data'
+
+interface IProps {
+    children?: ReactNode
+}
+
+const NavBar: FC<IProps> = (props) => {
+    return (
+        <NavWrapper>
+            <div className="nav wrap-v1">
+                {
+                discoverMenu.map(item=>{
+                    return (
+                        <div className="item" key={item.link}>
+                            <NavLink to={item.link}>
+                                {item.title}
+                            </NavLink>
+                        </div>
+                    )
+                })
+            }
+            </div>
+        </NavWrapper>
+    )
+}
+
+export default memo(NavBar)
