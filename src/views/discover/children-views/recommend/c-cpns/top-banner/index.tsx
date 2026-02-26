ac77bf1328c41fb40e9d8d8abf991b071799d4fe
JagerChoi <17322905680@qq.com>
2026-02-26 13:59:18 +0800

    推荐页面--轮播图三部分的搭建

--- src/views/discover/children-views/recommend/c-cpns/top-banner/index.tsx
+++ src/views/discover/children-views/recommend/c-cpns/top-banner/index.tsx
@@ -1,6 +1,8 @@
 import React, { memo } from 'react'
 import type { FC, ReactNode } from 'react'
 import {useAppSelector,shallowEqualApp} from '@/store'
+import {BannerControl, BannerLeft, BannerRight, BannerWrapper} from './style'
+import {Carousel} from 'antd'
 
 interface IProps {
     children?: ReactNode
@@ -12,11 +14,33 @@ const TopBanner: FC<IProps> = (props) => {
         banners:state.recommend.banners
     }),shallowEqualApp)
     return (
-    <div>
-        {banners.map((item)=>{
-            return <div key={item.imageUrl}>{item.imageUrl}</div>
-        })}
-    </div>)
+    <BannerWrapper>
+        <div className="banner wrap-v2">
+            <BannerLeft>
+            <Carousel autoplay>
+                    {
+                    banners.map(item=>{
+                        return(
+                            <div className='banner-item' key={item.imageUrl}>
+                                <img
+                                className="image"
+                                src={item.imageUrl}
+                                alt={item.typeTitle}/>
+                            </div>
+                        )
+                    })
+                    }
+            </Carousel>
+
+        </BannerLeft>
+        <BannerRight></BannerRight>
+        <BannerControl>
+            <button className="btn left"></button>
+            <button className="btn right"></button>
+        </BannerControl>
+        </div>
+    </BannerWrapper>
+    )
 }
 
 export default memo(TopBanner)
