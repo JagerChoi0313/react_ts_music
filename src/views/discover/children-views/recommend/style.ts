b36a29e3e9579d88ea61506f1ec65fbd0b044f1f
JagerChoi <17322905680@qq.com>
2026-02-26 23:25:20 +0800

    推荐页面--推荐页面内容的左右布局

---
+++ src/views/discover/children-views/recommend/style.ts
@@ -0,0 +1,18 @@
+import styled from 'styled-components'
+
+export const RecommendWrapper = styled.div`
+  .content {
+    border: 1px solid #d3d3d3;
+    display: flex;
+    >.left {
+      width: 732px;
+      padding: 20px;
+      border-right: 1px solid #d3d3d3;
+    }
+      >.right {
+        width: 250px;
+        margin-left: 1px;
+      }
+  }
+`
+
