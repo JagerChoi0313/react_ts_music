003ff7f04fcb1f1a72cabf4a9bd153cfb90f1f5f
JagerChoi <17322905680@qq.com>
2026-02-27 21:55:09 +0800

    推荐页面--热门推荐的数据和图片的格式化

--- src/components/song-menu-item/index.tsx
+++ src/components/song-menu-item/index.tsx
@@ -1,6 +1,8 @@
 import React, { memo } from 'react'
 import type { FC, ReactNode } from 'react'
 import {MenuItemWrapper} from './style'
+import {formatCount} from '@/utils/format'
+import {getImageSize} from '@/utils/format'
 
 interface IProps {
     children?: ReactNode
@@ -12,12 +14,14 @@ const SongMenuItem: FC<IProps> = (props) => {
     return (
     <MenuItemWrapper>
         <div className="top">
-            <img src={itemData.picUrl} alt='' />
+            <img src={getImageSize(itemData.picUrl,140)} alt='' />
             <div className="cover sprite_cover">
                 <div className="info">
                     <span>
                         <i className="sprite_icon headset"></i>
-                        <span className="count">{itemData.playCount}</span>
+                        <span className="count">
+                            {formatCount(itemData.playCount)}
+                        </span>
                     </span>
                     <i className="sprite_icon play"></i>
                 </div>
---
+++ src/utils/format.ts
@@ -0,0 +1,14 @@
+export function formatCount(count:number)
+{
+    if(count>100000)
+    {
+        return Math.ceil(count/10000) + '万'
+    }else{
+        return count
+    }
+}
+
+export function getImageSize(imageUrl:string,width:number,height:number=width)
+{
+    return imageUrl + `?param=${width}x${height}`
+}
