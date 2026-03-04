9d0bf08eb5c15e7997e01cfcbee8e6589879047d
JagerChoi <17322905680@qq.com>
2026-03-04 15:38:55 +0800

    推荐页面--用户登录区域的界面搭建

--- src/views/discover/children-views/recommend/index.tsx
+++ src/views/discover/children-views/recommend/index.tsx
@@ -8,6 +8,7 @@ import { RecommendWrapper } from './style'
 import HotRecommend from './c-cpns/hot-recommend'
 import NewAlbum from './c-cpns/new-album'
 import TopRanking from './c-cpns/top-ranking'
+import UserLogin from './c-cpns/user-login'
 
 
 interface IProps {
@@ -39,7 +40,11 @@ const Recommend: FC<IProps> = () => {
           <NewAlbum />
           <TopRanking />
         </div>
-        <div className="right">right</div>
+        <div className="right">
+          <UserLogin />
+          <div>入驻歌手</div>
+          <div>热门主播</div>
+        </div>
       </div>
 
     </RecommendWrapper>
