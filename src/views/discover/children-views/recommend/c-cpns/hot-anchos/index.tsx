c93926a4b460c0b16ab2f1b62de52d3e76a21e35
JagerChoi <17322905680@qq.com>
2026-03-04 19:06:40 +0800

    推荐页面--热门主播区域的页面展示

--- src/views/discover/children-views/recommend/c-cpns/hot-anchos/index.tsx
+++ src/views/discover/children-views/recommend/c-cpns/hot-anchos/index.tsx
@@ -1,6 +1,8 @@
 import React, { memo } from 'react'
 import type { FC, ReactNode } from 'react'
 import {AnchorWrapper} from './style'
+import {hotRadios} from '@/assets/data/local-data'
+import AreaHeaderV2 from '@/components/area-header-v2'
 
 interface IProps {
     children?: ReactNode
@@ -9,7 +11,26 @@ interface IProps {
 const HotAnchor: FC<IProps> = (props) => {
     return (
     <div>
-        
+        <AnchorWrapper>
+        <AreaHeaderV2/>
+        <div className="anchors">
+                {
+                    hotRadios.map((item)=>{
+                        return(
+                            <div className="item">
+                               <a href="" className="image">
+                                 <img src={item.picUrl} alt="" />
+                               </a>
+                                <div className="info">
+                                <div className="name">{item.name}</div>
+                                <div className="position">{item.position}</div>
+                                </div>
+                            </div>
+                        )
+                    })
+                }
+        </div>
+        </AnchorWrapper>
     </div>
     )
 }
